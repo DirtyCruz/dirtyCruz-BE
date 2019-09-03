0a1f507b4080fd1b02ddf9e8fd89a4f13e95fd8c
Dirt Deodara <thecrowleaps@gmail.com>
2019-09-03 15:58:11 -0700

    added a GET route and tested

--- test/app.test.js
+++ test/app.test.js
@@ -4,6 +4,7 @@ const request = require('supertest');
 const app = require('../lib/app');
 const connect = require('../lib/utils/connect');
 const mongoose = require('mongoose');
+const Note = require('../lib/models/Note');
 
 describe('app routes', () => {
   beforeAll(() => {
@@ -30,6 +31,23 @@ describe('app routes', () => {
           _id: expect.any(String),
           title: 'Up the Nerds!',
           body: 'I must not fear. Fear is the mind-killer...',
+          __v: 0
+        });
+      });
+  });
+
+  it('can GET all notes', async() => {
+    await Note.create([
+      { title: 'Wiz-Bang!', body: 'Whatseewhozit!' },
+      { title: 'Shumalumma', body: 'Ammulamuhs' }
+    ]);
+    return request(app)
+      .get('/api/v1/notes')
+      .then(res => {
+        expect(res.body).toContainEqual({
+          _id: expect.any(String),
+          title: 'Wiz-Bang!', 
+          body: 'Whatseewhozit!',
           __v: 0
         });
       });
